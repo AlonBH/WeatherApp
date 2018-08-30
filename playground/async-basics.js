1310f6e00271c26a82492b2c0d961f4b6faf0509
Alon Ben Haim <alon.b.h.672@gmail.com>
2018-08-30 18:22:14 +0300

    Section 4.2 - Async Basics

---
+++ playground/async-basics.js
@@ -0,0 +1,11 @@
+console.log('Starting app');
+
+setTimeout(() => {
+    console.log('We jump street, and we about to jump in yo ass');
+}, 2000);
+
+setTimeout(() => {
+    console.log('Right in the crack');
+}, 0);
+
+console.log('Finishing up');
